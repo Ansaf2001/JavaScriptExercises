a74836b35ac91ecd3e44e388c535e682b180db52
ansaf <ansafansar2001@gmail.com>
2024-07-30 13:39:26 +0530

    data type check in console

--- JS/Script.js
+++ JS/Script.js
@@ -74,7 +74,77 @@ console.log(test);
 
 const A = 73;
 console.log(A);
-A = 36; //constant error
+//A = 36; //constant error
 
 const student = {'name':"Ansaf",'Age':23};
 console.log(student);
+console.table(student);
+console.log(student.name);
+student.Age = 24;
+console.log(student);
+student.name = "Ansaf Ansar";
+console.table(student);
+
+
+//Data Types in JavaScript
+// JavaScript is a Dynamic Programming Language.
+
+// Number
+// Boolean
+// Null
+// undefined
+// Symbols  (Introduced in E6)
+
+//**Reference Types**
+// Array
+// Object Literals
+// Date
+
+let fname = "Ansaf";
+console.log(typeof fname);
+
+let age = 23;
+console.log(typeof age);
+
+var isMarried = true;
+console.log(typeof isMarried);
+
+var phone = null;
+console.log(typeof phone);
+
+let sampleText1;
+console.log(typeof sampleText1);
+
+
+//ES6 launched in 2015
+
+const s1=Symbol();
+console.log(s1);
+
+const s2=Symbol();
+console.log(s2);
+
+console.log(s1==s2);
+
+//references type
+
+var courses=['c','c++','java'];
+console.log(typeof courses);
+
+var student1={
+    'name': 'Ansaf',
+    'Age' : '23'
+}
+console.log(typeof student1);
+
+var date1=new Date();
+console.log(date1);
+console.log(typeof date1);
+
+//Type Conversion
+
+let a1;
+a1=25;
+console.log(a1, typeof a1);
+a1=String(a1);
+console.log(a1, typeof a1);
